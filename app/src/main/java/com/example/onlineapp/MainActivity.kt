f71890564f7d701ff8e38d5b14cc700d55d20acf
safi <safihamzaprog7869@gmail.com>
2023-03-20 03:43:48 -0700

    this first user app commit

---
+++ app/src/main/java/com/example/onlineapp/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.onlineapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.PopupMenu
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.fragment.findNavController
+import com.example.onlineapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val nevHostFragment=supportFragmentManager.findFragmentById(R.id.fragmentContainer)
+        val navController=nevHostFragment!!.findNavController()
+
+        val popupMenu=PopupMenu(this,null)
+        popupMenu.inflate(R.menu.bottom_nav)
+        binding.bottomBar.setupWithNavController(popupMenu.menu,navController)
+        navController.addOnDestinationChangedListener(object :NavController.OnDestinationChangedListener{
+            override fun onDestinationChanged(
+                controller: NavController,
+                destination: NavDestination,
+                arguments: Bundle?
+            ) {
+                title=when(destination.id){
+                    R.id.cardFragment -> "My Cart"
+                    R.id.moreFragment -> "My Deshboard"
+                    else -> "onlineApp"
+                }
+            }
+
+        })
+    }
+}
